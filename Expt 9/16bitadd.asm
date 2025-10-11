dc89eb5fe67b7e31e9a08a242a2ec31bbe94690e
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2025-10-11 11:12:24 +0530

    Create 16bitadd.asm

---
+++ Expt 9/16bitadd.asm
@@ -0,0 +1,40 @@
+```asm
+CODE SEGMENT
+ASSUME CS:CODE
+START:
+    MOV CL, 00H        ; Initialize carry indicator
+    MOV SI, 3000H      ; Set starting memory address
+
+    ; Load first 16-bit number into AX
+    MOV AL, [SI]
+    INC SI
+    MOV AH, [SI]
+    INC SI
+
+    ; Load second 16-bit number into BX
+    MOV BL, [SI]
+    INC SI
+    MOV BH, [SI]
+
+    ; Add the two 16-bit numbers
+    ADD AX, BX
+
+    ; Store result at [SI+4]
+    INC SI
+    MOV [SI], AL
+    INC SI
+    MOV [SI], AH
+
+    ; Check for carry
+    JC L1
+    INC SI
+    MOV [SI], CL      ; No carry, store 0
+    INT 3             ; Breakpoint
+L1:
+    INC CL
+    INC SI
+    MOV [SI], CL      ; Carry occurred, store 1
+    INT 3             ; Breakpoint
+
+CODE ENDS
+END START
